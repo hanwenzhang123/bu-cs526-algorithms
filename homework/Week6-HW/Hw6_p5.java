42a40580c4e1227a90b6dca91a600ecb8d8aba72
Hanwen Zhang <hanwenzhang888@gmail.com>
2022-12-11 21:00:32 -0500

    Add files via upload

--- homework/Week6-HW/Hw6_p5.java
+++ homework/Week6-HW/Hw6_p5.java
@@ -3,28 +3,28 @@ import java.util.*;
 
 public class Hw6_p5 {
     public static void main(String[] args) {
-        ArrayList<Node> adjList = new ArrayList<>();
+        ArrayList<Node> adjList = new ArrayList<>(); // Store the follow relationships in an adjacency list that implemented as an ArrayList of nodes.
 
         // read the file and create relationship list
         File file = new File("follows_input.txt");
 
         try {
             FileReader reader = new FileReader(file);
-            BufferedReader br = new BufferedReader(reader);
+            BufferedReader bufferedReader = new BufferedReader(reader);
 
             // read the file line by line
             String line;
-            while ((line = br.readLine()) != null) {
+            while ((line = bufferedReader.readLine()) != null) {
 
                 List<String> strList = Arrays.asList(line.split(", "));
 
                 String firstKey = strList.get(0);
                 Node firstNode = getKeyNode(firstKey, adjList);
-                if (firstNode == null) {
-                    firstNode = new Node(firstKey);
+                if (firstNode == null) {        //if we have not had the key in the list yet
+                    firstNode = new Node(firstKey);     //create the node and assign to firstNode variable
                 }
 
-                // create relationship node to the adjacency list with relationships
+                // create relationship node to the adjacency list with relationships by adding edges to the first node
                 for (int i = 1; i < strList.size(); i++) {
                     String key = strList.get(i);
                     Node keyNode = getKeyNode(key, adjList);
@@ -37,8 +37,9 @@ public class Hw6_p5 {
                 // add relationship node to adjacency list
                 adjList.add(firstNode);
             }
-            br.close();
+            bufferedReader.close(); //close bufferedReader
 
+            //print out all the directly follows and indirectly follows
             allFollows("A", adjList);
             allFollows("B", adjList);
             allFollows("C", adjList);
@@ -51,15 +52,40 @@ public class Hw6_p5 {
             System.out.println(e);
         }
     }
-
-    public static String removeSuffix(String s, String suffix) {    //Serves as a delimiter for the string set
-        if (s.endsWith(suffix)) {
-            return s.substring(0, s.length() - suffix.length());
+    public static Node getKeyNode(String name, ArrayList<Node> adjList) {  //Find the key node
+        for (int i = 0; i < adjList.size(); i++) {
+            if (adjList.get(i).name.equals(name)) {
+                return adjList.get(i);
+            } else {
+                ArrayList<Node> subList = adjList.get(i).list;
+                for (int j = 0; j < subList.size(); j++) {
+                    if (subList.get(j).name.equals(name)) {
+                        return subList.get(j);
+                    }
+                }
+            }
         }
-        return s;
+        return null;
     }
+    public static void allFollows(String name, ArrayList<Node> adjList) {  //Follow the direct and indirect relationships between nodes
+        for (int i = 0; i < adjList.size(); i++) {
+            if (adjList.get(i).name.equals(name)) {
 
-    static String getSetString(HashSet<String> list) {
+                // direct follows
+                HashSet<String> direct = adjList.get(i).follows();
+                System.out.println(name + " directly follows " + getString(direct));
+
+                // indirect follow
+                HashSet<String> indirect = adjList.get(i).indirectFollows();
+                indirect.removeAll(direct);     //remove the direct ones
+                indirect.remove(name);      //remove the first node itself
+                System.out.println(name + " indirectly follows " + getString(indirect));
+
+                break;
+            }
+        }
+    }
+    public static String getString(HashSet<String> list) {
         String str = "{";
         for (String nm : list) {
             str += nm + ", ";
@@ -68,37 +94,11 @@ public class Hw6_p5 {
         str += "}";
         return str;
     }
-    static void allFollows(String name, ArrayList<Node> adjList) {  //Follow the direct and indirect relationships between nodes
-        for (int i = 0; i < adjList.size(); i++) {
-            if (adjList.get(i).name.equals(name)) {
 
-                // direct follows
-                HashSet<String> direct = adjList.get(i).follows();
-                System.out.println(name + " directly follows " + getSetString(direct));
-
-                // indirect follow
-                HashSet<String> indirect = adjList.get(i).indirectFollows();
-                indirect.removeAll(direct);
-                indirect.remove(name);
-                System.out.println(name + " indirectly follows " + getSetString(indirect));
-
-                break;
-            }
+    public static String removeSuffix(String s, String suffix) {    //Serves as a delimiter for the string set
+        if (s.endsWith(suffix)) {
+            return s.substring(0, s.length() - suffix.length());
         }
-    }
-    static Node getKeyNode(String name, ArrayList<Node> adjList) {  //Find the key node
-        for (int i = 0; i < adjList.size(); i++) {
-            if (adjList.get(i).name.equals(name)) {
-                return adjList.get(i);
-            } else {
-                ArrayList<Node> sublist = adjList.get(i).list;
-                for (int j = 0; j < sublist.size(); j++) {
-                    if (sublist.get(j).name.equals(name)) {
-                        return sublist.get(j);
-                    }
-                }
-            }
-        }
-        return null;
+        return s;
     }
 }
--- homework/Week6-HW/Node.java
+++ homework/Week6-HW/Node.java
@@ -1,8 +1,8 @@
 import java.util.*;
 
 public class Node {
-    String name;
-    ArrayList<Node> list;
+    String name;    //the name of the person
+    ArrayList<Node> list;   //a reference include all people the person directly follows.
 
     Node(String name) {
         this.name = name;
@@ -14,20 +14,18 @@ public class Node {
     }
 
     public HashSet<String> follows() {  //Direct follows
-        HashSet<String> setNode = new HashSet<>();
-
+        HashSet<String> setNode = new HashSet<>();  //use set to keep uniqueness
         if (list.size() == 0) {
             return setNode;
         }
-
         for (int i = 0; i < list.size(); i++) {
             setNode.add(list.get(i).name);
         }
         return setNode;
     }
-    
+
     public HashSet<String> indirectFollows() {  //Indirect Follows
-        HashSet<String> setNode = new HashSet<>();
+        HashSet<String> setNode = new HashSet<>();  //use set to keep uniqueness
         setNode.add(name);
         for (int i = 0; i < list.size(); i++) {
             setNode.addAll(list.get(i).indirectFollows());
